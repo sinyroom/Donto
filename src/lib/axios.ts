3767365b6d8c3a3aaf9c2e13ea10baf856e68c51
sinyroom <wldmsdl7913@naver.com>
2025-12-16 13:43:07 +0900

    chore: axios 인스턴스, cn 유틸함수 추가

---
+++ src/lib/axios.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
