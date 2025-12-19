43d06640c3599999c41174f30679f246249eef05
sinyroom <wldmsdl7913@naver.com>
2025-12-19 16:06:57 +0900

    feat(icon): asset 추가

---
+++ src/components/project/SortSelect.tsx
@@ -0,0 +1,10 @@
+export default function SortSelect() {
+  return (
+    <div>
+      <select className="text-sm border border-gray-200 bg-white rounded px-2 py-1">
+        <option>최신순</option>
+        <option>생성일순</option>
+      </select>
+    </div>
+  );
+}
